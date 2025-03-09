01da69f7ed808adf54c235e8901b681d261bfafb
Kradnuzz <grand_razor98@yahoo.com>
2025-03-09 23:30:04 +0700

    1-4- Ini Masih tes

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Halo Gais</h1>
+      <h1>Halo Gais</h1> // Jonathan Harun
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
