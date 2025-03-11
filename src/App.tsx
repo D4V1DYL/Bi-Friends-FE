493121e3730a53e2ede369e7d270b36db315b6d6
Kradnuzz <grand_razor98@yahoo.com>
2025-03-12 02:34:35 +0700

    1-31-LoadingPage-And Maintainance Page
    - Add Maintainance Page
    - Add TailwindCSS
    - Add new Asset

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import MaintainanceScreen from './Components/MaintainanceComponent/MaintananceScreen';
 import LoginPage from './LoginFolder/LoginPage'
 import RegisterPage from './RegisterFolder/RegisterPage'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/RegisterPage" element={<RegisterPage />} />
+        <Route path="/404" element={<MaintainanceScreen />} />
       </Routes>
     </Router>
   );
