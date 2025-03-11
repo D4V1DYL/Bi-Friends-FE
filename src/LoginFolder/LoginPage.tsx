ee27c4da88dc711fe88700a7c8969616b53bd3f7
Calvin A <68487872+calvin1237@users.noreply.github.com>
2025-03-11 18:25:02 +0700

    1 - 6A (Front end Form untuk username dan password) - 7A (Membuat Front end Button untuk Login pada Login Page) 8A - (Front end link “Forgot Password” yang mengarahkan ke form pemulihan)
    Bikin FrontEnd:
    - Username Input
    - Password Input
    - Button Login
    - Forgot Password
    - Register new account

---
+++ src/LoginFolder/LoginPage.tsx
@@ -0,0 +1,67 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import "./LoginPage.css"
+
+function LoginPage(){
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      document.body.classList.add("RegisterPage");
+      return () => document.body.classList.remove("RegisterPage"); 
+    }, []);
+
+
+    function handleLogin() {
+      navigate("/RegisterPage");
+    }
+
+    return(
+            <div id="TheTemplate">
+                <div className="TheBox">
+                    <h2 className="LoginTxt">Login to your account</h2>
+                    
+                    <div className="Txt_Input_Box">
+                        <p className="InputTxt">Username</p>
+                        <input className="Username" type="Username"></input>
+                    </div>
+
+                    <br></br>
+
+                    <div className="Txt_Input_Box">
+                        <p className="InputTxt">Password</p>
+                        <input className="Password" type="Password"></input>
+                    </div>
+
+
+                    <div className="RememberBox_ForgetPassword_Box">
+                        <div className="RememberBox">
+                            <input id="RememberMe" type="checkbox"></input>
+                            <p id="RememberMeTxt" >Remember me</p>
+                        </div>
+
+                        <div className="ForgetPasswordBox">
+                            <a id="ForgetPassword" href="#">Forget Password?</a>
+                        </div>
+                    </div>
+
+
+
+
+                    <div className="ButtonBox">
+                        <button className="LoginBtn">Login</button>
+                    </div>
+
+
+                    <div className="RegisterBox">
+                        <a className="Register" href="#" onClick={handleLogin}>Create your account</a>
+                    </div>
+
+                </div>
+                <div id="TheImage">
+                    <img src="./src/assets/login.png" alt="loginImg" />
+                </div>
+            </div>
+    );
+}
+export default LoginPage
