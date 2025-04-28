7ea41a21cef2ff2dbc81e234abcb2f60a70ef4d9
Asahi7208 <joshuarobert798@gmail.com>
2025-04-29 02:28:24 +0700

    2-T016A
    -. LoginPage sudah redirect ke HomePage apabila user login
    -. Tambahin profile dan chat page (belum ada isi)
    -. Buat komponen navigation bar (Profile Icon dan Message Icon sudah redirect)
    -. Home page isinya nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import LoginPage from './LoginFolder/LoginPage'
 import RegisterPage from './RegisterFolder/RegisterPage'
 import ForgotPassword from './ForgotPasswordFolder/ForgotPasswordPage'
 import RecoveryPassword from './RecoveryPasswordFolder/RcoveryPasswordPage'
+import HomePage from './HomeFolder/HomePage';
+import ProfilePage from './ProfileFolder/ProfilePage';
+import ChatPage from './ChatFolder/ChatPage';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -12,9 +15,12 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/ProfilePage" element={<ProfilePage />} />
         <Route path="/RegisterPage" element={<RegisterPage />} />
         <Route path="/ForgotPassword" element={<ForgotPassword />} />
         <Route path='/RecoveryPassword' element={<RecoveryPassword />} />
+        <Route path="/HomePage" element={<HomePage />} />
+        <Route path="/ChatPage" element={<ChatPage />} />
         <Route path="/404" element={<MaintainanceScreen />} />
       </Routes>
     </Router>
---
+++ src/HomeFolder/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavigationBar from '../Components/NavigationComponent/NavigationBar';
+import './HomePage.css';
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="homepage">
+      <NavigationBar />
+    </div>
+  );
+};
+
+export default HomePage;
