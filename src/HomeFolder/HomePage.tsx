035f0af2bfc6bba5463b2af50dd7382af78c1f7e
LOUlou1012 <michaellou363@gmail.com>
2025-04-30 13:49:48 +0700

    3-21B
    buat textbox tempat input

--- src/HomeFolder/HomePage.tsx
+++ src/HomeFolder/HomePage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import NavigationBar from '../Components/NavigationComponent/NavigationBar';
 import EventWidget from '../Components/EventWidgetComponent/EventWidget';
 import './HomePage.css';
+import profile from '../assets/profileLogo.png';
 
 const HomePage: React.FC = () => {
   return (
@@ -9,7 +10,32 @@ const HomePage: React.FC = () => {
       <NavigationBar />
       <div className="homepage-content">
         <div className="main-content">
-          {/* Forum nanti disini */}
+
+
+
+
+          <div className='inputForm'>
+          
+            <div className='divAtas'>
+
+              <img src={profile} 
+                alt="Profile"
+                className="profile-icon">
+              </img>
+              <input type="text" id="textinput" placeholder="What's new?" />
+
+            </div>
+            
+            <div className='divBawah'>
+              <button id="submitButton">post</button>
+            </div>
+
+          </div>
+
+
+
+
+
         </div>
         <div className="event-section">
           <h3 className="event-title">EVENT</h3>
