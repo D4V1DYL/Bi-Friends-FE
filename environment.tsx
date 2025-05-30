b8706f6faa8dd1206b282b12e2da1c06a2fcd0bc
Jonathan05 <jojoharun1@gmail.com>
2025-05-30 15:33:01 +0700

    Update environment
    Ganti link

--- environment.tsx
+++ environment.tsx
@@ -1,2 +1,2 @@
-// export const baseURL = 'https://bifriendsbe.bifriends.my.id/';
-export const baseURL = 'http://127.0.0.1:8000/'
+export const baseURL = 'https://bifriendsbe.bifriends.my.id/';
+// export const baseURL = 'http://127.0.0.1:8000/'
